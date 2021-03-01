9aeec63e497a78873c02d5ac58308893aae050e2
isabellavjs <isabellavjs@gmail.com>
2021-03-01 15:04:51 -0300

    First commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = 3000;
 
-// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_req, res) => res.send('Hello World!'));
+
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(port, () => console.log(`Example app listening on ${port}!`));
