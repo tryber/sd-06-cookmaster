426f5c2db6262fdfe0ccb5c8eff8a61228194f14
LeonardoCavachini <cavachini240@gmail.com>
2021-03-01 18:59:39 -0300

    fixing server

--- model/connection.js
+++ model/connection.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-//const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 const connection = () => {
-  return MongoClient
+  const server = MongoClient
     .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -14,6 +14,7 @@ const connection = () => {
       console.error(err);
       process.exit(1);
     });
+  return server;
 };
 
 module.exports = connection;
