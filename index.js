e0c67b6ea2102fae0d59c45968a471880f75d1b0
Isa Koga <isadorakoga710@gmail.com>
2021-03-01 15:58:32 -0300

    :rotating_light: Linter fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 const port = 3000;
@@ -13,6 +14,6 @@ const { UsersRouter } = require('./Controller/usersController');
 
 app.use(bodyParser.json());
 
-app.use('/users', UsersRouter)
+app.use('/users', UsersRouter);
 
 app.listen(port, () => console.log(`Listening to ${port}`));
