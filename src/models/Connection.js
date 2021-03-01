daa126ba5f241ca297672bda18857ee68341e269
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-03-01 15:51:58 -0300

    erro delint ja

--- src/models/Connection.js
+++ src/models/Connection.js
@@ -5,15 +5,13 @@ const MONGO_DB_URL = process.env.IS_LOCAL
   ? 'mongodb://localhost:27017/Cookmaster'
   : 'mongodb://mongodb:27017/Cookmaster';
 
-  const DB_NAME = 'Cookmaster';
+const DB_NAME = 'Cookmaster';
 
-exports.connection = () => {
-  return MongoClient.connect(MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then((conn) => conn.db(DB_NAME))
+exports.connection = () => MongoClient.connect(MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((conn) => conn.db(DB_NAME))
   .catch((err) => {
     console.error(err);
     process.exit(1);
   });
-};
