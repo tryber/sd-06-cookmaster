63594888b6fc424f3c0085582f52cded9f96fa89
deko possas <andrepossaster@gmail.com>
2021-03-01 16:07:29 -0300

    baby staps for index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use('/users', UserRouter);
+
+app.listen(port, () => console.log(`listening to ${port}`));
