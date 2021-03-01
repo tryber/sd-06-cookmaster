5532191d610101fa33dd2cfd6332d041c76b9096
Rodrigo Leite <rods.leite27@gmail.com>
2021-03-01 15:00:37 -0300

    iniciando projeto

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+// /images é o caminho da API onde as imagens estarão disponíveis
+// path.join(__dirname, 'uploads') é o caminho da pasta onde o multer salva suas imagens ao realizar o upload
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
+
+
