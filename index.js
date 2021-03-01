1cad1f50f13083eefc03c13c369050addb0a822b
Paulo Frazão <paulo4187@gmail.com>
2021-03-01 18:52:41 -0300

    forcing linter error

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => { 
+app.get(' /',  (request, response) => { 
   response.send();
 });
