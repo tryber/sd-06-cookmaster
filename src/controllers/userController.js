0f556e8a95d4819e5e183f78abd4a49a92c40d8b
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-03-01 18:53:59 -0300

    Requisito 1 nãopassou no local, mas OK no Insominia

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,19 @@
 const service = require('../services/serviceUser');
 const {
   created,
+  conflict,
+  badRequest,
 } = require('../utils/messages');
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
-  const newUser = await service.userCreate(name, email, password);
+  const role = 'user';
+  if (!name || !email || !password) {
+    return res.status(badRequest).json({ message: 'Invalid entries. Try again.' });
+   }
+  const emailNoExist = await service.emailExist(name, email, password);
+  if (!emailNoExist) return res.status(conflict).json({ message: 'Email already registered' });
+  const newUser = await service.userCreate(name, email, password, role);
   res.status(created).json(newUser);
 };
 
--- src/services/serviceUser.js
+++ src/services/serviceUser.js
@@ -1,14 +1,14 @@
 const model = require('../models/cookModelUsers');
 const { zero } = require('../utils/messages');
 
-const userCreate = async (name, email, password) => {
-  const user = await model.createUser(name, email, password);
+const userCreate = async (name, email, password, role) => {
+  const user = await model.createUser(name, email, password, role);
   return user;
 };
 
-const emailExist = async (_name, email, _passaword) => {
-  const qetAllUsers = await model.getAllUsers;
-  const emails = qetAllUsers.filter((user) => user.email === email);
+const emailExist = async (_name, email, _password) => {
+  const emaildobanco = await model.getAllUsers();
+  const emails = emaildobanco.filter((user) => user.email === email);
   if (emails.length > zero) return false;
   return true;
 };
