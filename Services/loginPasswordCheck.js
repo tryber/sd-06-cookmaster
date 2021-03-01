6d2c22e19e4e8f353ba1ca106c6ffbc490f36259
deko possas <andrepossaster@gmail.com>
2021-03-01 20:11:57 -0300

    criei um validador de password do login

---
+++ Services/loginPasswordCheck.js
@@ -0,0 +1,14 @@
+const { findOneUser } = require('../Model/usersModel');
+
+const getUserByEmail = async (email) => findOneUser(email);
+
+const loginPasswordCheck = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await getUserByEmail(email);
+  if (!user || user.password !== password) {
+    return res.status(401).json({ message: 'Incorrect username or password' });
+  }
+  next();
+};
+
+module.exports = { loginPasswordCheck };
