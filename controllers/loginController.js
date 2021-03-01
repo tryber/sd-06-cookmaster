81720d56a1ae71336e463f464986389c74d9758e
Marcelo Pessini <marceloppessini@gmail.com>
2021-03-01 16:55:36 -0300

    Requisito 02

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -14,7 +14,8 @@ const SUCCESS = 200;
 loginRouter.post('/', validateLogin, userExists, async (request, response) => {
   const { email } = request.body;
   const user = await usersModel.getOneUser(email);
-  const userPayload = { id: user.id, email: user.email, role: user.role };
+  const { _id } = user;
+  const userPayload = { id: _id, email: user.email, role: user.role };
   const token = jwt.sign(userPayload, '1234', { algorithm: 'HS256' });
   return response.status(SUCCESS).json({ token });
 });
