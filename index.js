a4dd0336f3dfcc7ff12506c743860099baeccb9a
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-03-01 18:24:48 -0300

    Opening the PR

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
