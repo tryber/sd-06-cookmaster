67e59d7addeb697b408c46d2ece8bc80948bce0c
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-03-01 15:44:52 -0300

    first commit

---
+++ src/models/Connection.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.IS_LOCAL
+  ? 'mongodb://localhost:27017/Cookmaster'
+  : 'mongodb://mongodb:27017/Cookmaster';
+
+  const DB_NAME = 'Cookmaster';
+
+exports.connection = () => {
+  return MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then((conn) => conn.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+};
