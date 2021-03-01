462869b00890392ece9c7288c24f9ff3c3e2f1b6
Cássio Rubens <cassiorubensadv@gmail.com>
2021-03-01 19:12:20 -0300

    FEAT: 1 - Crie um endpoint para o cadastro de usuários

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const UsersController = require('./controllers/UsersController');
 
 const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+// 1 
+app.use('/users', UsersController);
+// 1
+
+app.listen(port, () => console.log(`Start http://localhost:${port}`));
---
+++ models/Users.js
@@ -0,0 +1,16 @@
+const connection = require('./connection');
+
+const getAll = async () => connection().then((db) => db.collection('users').find().toArray());
+
+// No banco um usuário precisa ter os campos Email, Senha, Nome e Role.
+const create = async (name, email, password, role) => 
+  connection().then((db) => db.collection('users').insertOne({ name, email, password, role }));
+
+const getByEmail = async (email) =>
+  connection().then((db) => db.collection('users').findOne({ email }));
+
+module.exports = {
+  getAll,
+  create,
+  getByEmail,
+};
