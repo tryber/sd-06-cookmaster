b369d6d5d6b66513b0fc8b2f72ca62d6f0b314ee
Luiz Mello <luizmacmillan@gmail.com>
2021-03-01 19:17:29 -0300

    Iniciando projeto.

---
+++ models/connection.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+
+const MONGODB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const MONGODB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DATABASE = 'Cookmaster';
+
+const connection = async () => {
+  const result = await MongoClient.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then((conn) => conn.db(DATABASE))
+    .catch((err) => {
+      console.error(err);
+      process.exit(err);
+    });
+  return result;
+};
+
+module.exports = connection;
