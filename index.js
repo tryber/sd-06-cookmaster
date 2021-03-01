644fcd5e3ba952db6479ae51fa32dda4c9e902eb
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-03-01 15:34:37 -0300

    iniciando
    iniciando

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-// não remova esse endpoint, e para o avaliador funcionar
+// não remova esse endpoint, e para o avaliador funcionar.
 app.get('/', (request, response) => {
   response.send();
 });
