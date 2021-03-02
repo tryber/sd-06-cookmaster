ef01fdbcfd4815d6fd3133631293c0295a4f18ff
deko possas <andrepossaster@gmail.com>
2021-03-02 18:36:12 -0300

    testando o recipes router

--- Model/recipeModel.js
+++ Model/recipeModel.js
@@ -2,22 +2,22 @@ const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const findAllRecipes = async () => connection()
-  .then((db) => db.connection('recipes').find().toArry());
+  .then((db) => db.collection('recipes').find().toArray());
 
 const createRecipes = async (data) => connection()
-  .then((db) => db.connection('recipes').insertOne(data));
+  .then((db) => db.collection('recipes').insertOne(data));
 
 const findOneRecipes = async (id) => connection()
-  .then((db) => db.connection('recipes').findOne(ObjectId(id)));
+  .then((db) => db.collection('recipes').findOne(ObjectId(id)));
 
 const editRecipes = async (id, name, ingr, prep) => connection
-  .then((db) => db.connection('recipes').findAndUpdate(
+  .then((db) => db.collection('recipes').findAndUpdate(
     { _id: ObjectId(id) },
     { $set: { name, ingr, prep } },
   ));
 
 const deleteRecipes = async (id) => connection
-  .then((db) => db.connection('recipes').deleteOne({ _id: ObjectId(id) }));
+  .then((db) => db.collection('recipes').deleteOne({ _id: ObjectId(id) }));
 
 module.exports = {
   findAllRecipes,
--- controller/recipeController.js
+++ controller/recipeController.js
@@ -13,9 +13,17 @@ const RecipeRouter = new Router();
 
 const RESOLVE = 200;
 
-RecipeRouter.get('/', async (req, res) => {
+RecipeRouter.get('/', async (_req, res) => {
   const allRecipes = await getAllRecipes();
   res.status(RESOLVE).json(allRecipes);
+});
+
+RecipeRouter.get('/:id', validateId, async (req, res) => {
+  const { id } = req.params;
+  const recipe = await getRecipesById(id);
+  if (!recipe) {
+    return res.status(RESOLVE).json({ message: 'recipe not found' });
+  }
 });
 
 module.exports = { RecipeRouter };
