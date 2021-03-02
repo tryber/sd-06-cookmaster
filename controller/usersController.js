a7dff0759ddf9883cb60082989ccd1506e0f5579
Usuario <mguimaraes.carneiro@gmail.com>
2021-03-02 19:23:00 -0300

    Cookmaster project | Requirement 01

---
+++ controller/usersController.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+const {
+  createNewUser,
+  validateUser,
+  checkUniqueEmail,
+  getUsers,
+} = require('../services/usersServices');
+
+const UsersRouter = new Router();
+
+const twoHundred = 200;
+const twoHundredOne = 201;
+
+UsersRouter.get('/', async (req, res) => {
+  const allUsers = await getUsers();
+  res.status(twoHundred).json(allUsers);
+});
+
+UsersRouter.post('/', validateUser, checkUniqueEmail, async (req, res) => {
+  const user = { ...req.body, role: 'user' };
+  await createNewUser(user);
+  return res.status(twoHundredOne).json({ user });
+});
+
+module.exports = { UsersRouter }; 
--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const bodyParser = require('body-parser');
+const { UsersRouter } = require('./controller/usersController');
+
+app.use(bodyParser.json());
+
+app.use('/users', UsersRouter);
+
+app.listen(port, () => console.log(`Listening to ${port}`));
