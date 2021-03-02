af1189b75fab1891f0d9f1dbf937afd78ad4b46d
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-03-02 13:08:22 -0300

    Refactoring code: improved authentication

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,21 +1,18 @@
 const usersModel = require('../models/usersModel');
 const verifyToken = require('../data/verifyToken');
-const { invalidToken, missingToken } = require('../utils/errorsLibrary');
-const { UNAUTHORIZED } = require('../utils/statusCode');
+const { invalidToken, missingToken, invalidEntry } = require('../utils/errorsLibrary');
 
 module.exports = async (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) res.status(UNAUTHORIZED).json({ message: missingToken.message });
-
   try {
+    const token = req.headers.authorization;
+    if (!token) await Promise.reject(missingToken);
+
     const payload = verifyToken(token);
-    if (payload.name === 'JsonWebTokenError') {
-      res.status(UNAUTHORIZED).json({ message: invalidToken.message });
-    }
+    if (!payload.data) await Promise.reject(invalidToken);
 
     const { data: { email } } = payload;
     const isUserValid = await usersModel.getByEmail(email);
-    if (!isUserValid) res.status(UNAUTHORIZED).json({ message: invalidToken.message });
+    if (!isUserValid) await Promise.reject(invalidEntry);
 
     req.user = email;
     return next();
