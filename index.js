5a27dc50fe10ebdaad24b0cfdb53fc14321b6512
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-03-02 15:54:15 -0300

    Initial setup

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
 
 const app = express();
+const LOCALHOST_PORT = 3000;
+const PORT = process.env.PORT || LOCALHOST_PORT;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.get('/ping', (request, response) => {
+  response.status(200).send('pong!');
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
