5b42ba21f447018bb8c3b25191970814909332c9
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-03-02 17:04:21 -0300

    Commented recipes for now

--- services/recipes.js
+++ services/recipes.js
@@ -1,124 +1,124 @@
-const products = require('../models/products');
+// const products = require('../models/products');
 
-const minNameLength = 5;
-const nullQuantity = 0;
-const idMongoLength = 24;
-const nameLengthErrorMessage = '"name" length must be at least 5 characters long';
-const nameExists = 'Product already exists';
-const quantityErrorMessage = '"quantity" must be larger than or equal to 1';
-const quantityTypeErrorMessage = '"quantity" must be a number';
+// const minNameLength = 5;
+// const nullQuantity = 0;
+// const idMongoLength = 24;
+// const nameLengthErrorMessage = '"name" length must be at least 5 characters long';
+// const nameExists = 'Product already exists';
+// const quantityErrorMessage = '"quantity" must be larger than or equal to 1';
+// const quantityTypeErrorMessage = '"quantity" must be a number';
 
-const isValid = (name, quantity) => {
-  if (name.length < minNameLength) return nameLengthErrorMessage;
-  if (!Number.isInteger(quantity)) return quantityTypeErrorMessage;
-  if (quantity <= nullQuantity) return quantityErrorMessage;
+// const isValid = (name, quantity) => {
+//   if (name.length < minNameLength) return nameLengthErrorMessage;
+//   if (!Number.isInteger(quantity)) return quantityTypeErrorMessage;
+//   if (quantity <= nullQuantity) return quantityErrorMessage;
 
-  return true;
-};
+//   return true;
+// };
 
-const isUnique = async (name) => {
-  const checkUnique = await products.findByName(name);
+// const isUnique = async (name) => {
+//   const checkUnique = await products.findByName(name);
 
-  if (checkUnique) return nameExists;
+//   if (checkUnique) return nameExists;
 
-  return true;
-};
+//   return true;
+// };
 
-const getAll = async () => {
-  const productsArray = await products.getAll();
+// const getAll = async () => {
+//   const productsArray = await products.getAll();
 
-  return productsArray;
-};
+//   return productsArray;
+// };
 
-const findById = async (id) => {
-  const errorObject = {
-    err: {
-      code: 'invalid_data',
-      message: 'Wrong id format',
-    }
-  };
+// const findById = async (id) => {
+//   const errorObject = {
+//     err: {
+//       code: 'invalid_data',
+//       message: 'Wrong id format',
+//     }
+//   };
 
 
-  if (id.length !== idMongoLength) return errorObject;
+//   if (id.length !== idMongoLength) return errorObject;
 
-  const product = await products.findById(id);
+//   const product = await products.findById(id);
 
-  if (!product) return errorObject;
+//   if (!product) return errorObject;
   
-  return product;
-};
+//   return product;
+// };
 
-const create = async (productName, quantity) => {
-  const validOrErrorMessage = isValid(productName, quantity);
-  const isUniqueOrError = await isUnique(productName);
-  if (validOrErrorMessage !== true) return {
-    err: {
-      code: 'invalid_data',
-      message: validOrErrorMessage,
-    }
-  };
+// const create = async (productName, quantity) => {
+//   const validOrErrorMessage = isValid(productName, quantity);
+//   const isUniqueOrError = await isUnique(productName);
+//   if (validOrErrorMessage !== true) return {
+//     err: {
+//       code: 'invalid_data',
+//       message: validOrErrorMessage,
+//     }
+//   };
 
-  if (isUniqueOrError !== true) return {
-    err: {
-      code: 'invalid_data',
-      message: isUniqueOrError,
-    }
-  };
+//   if (isUniqueOrError !== true) return {
+//     err: {
+//       code: 'invalid_data',
+//       message: isUniqueOrError,
+//     }
+//   };
 
-  const { insertedId } = await products.create({ name: productName, quantity });
+//   const { insertedId } = await products.create({ name: productName, quantity });
 
-  return {
-    _id: insertedId,
-    name: productName,
-    quantity
-  };
-};
+//   return {
+//     _id: insertedId,
+//     name: productName,
+//     quantity
+//   };
+// };
 
-const update = async (id, updateProduct) => {
-  const { name, quantity } = updateProduct;
-  const validOrErrorMessage = isValid(name, quantity);
-  const errorObject = {
-    err: {
-      code: 'invalid_data',
-      message: 'Wrong id format',
-    }
-  };
+// const update = async (id, updateProduct) => {
+//   const { name, quantity } = updateProduct;
+//   const validOrErrorMessage = isValid(name, quantity);
+//   const errorObject = {
+//     err: {
+//       code: 'invalid_data',
+//       message: 'Wrong id format',
+//     }
+//   };
 
-  if (validOrErrorMessage !== true) return {
-    err: {
-      code: 'invalid_data',
-      message: validOrErrorMessage,
-    }
-  };
+//   if (validOrErrorMessage !== true) return {
+//     err: {
+//       code: 'invalid_data',
+//       message: validOrErrorMessage,
+//     }
+//   };
 
-  const updatedProduct = await products.update(id, name, quantity);
+//   const updatedProduct = await products.update(id, name, quantity);
 
-  if (!updatedProduct) return errorObject;
+//   if (!updatedProduct) return errorObject;
 
-  return updatedProduct;
-};
+//   return updatedProduct;
+// };
 
-const deleteProduct = async (id) => {
-  const errorObject = {
-    err: {
-      code: 'invalid_data',
-      message: 'Wrong id format',
-    }
-  };
+// const deleteProduct = async (id) => {
+//   const errorObject = {
+//     err: {
+//       code: 'invalid_data',
+//       message: 'Wrong id format',
+//     }
+//   };
 
-  if (id.length !== idMongoLength) return errorObject;
+//   if (id.length !== idMongoLength) return errorObject;
 
-  const deleteProduct = await products.deleteProduct(id);
+//   const deleteProduct = await products.deleteProduct(id);
   
-  if (!deleteProduct) return errorObject;
+//   if (!deleteProduct) return errorObject;
 
-  return deleteProduct;
-};
+//   return deleteProduct;
+// };
 
-module.exports = {
-  getAll,
-  findById,
-  create,
-  update,
-  deleteProduct,
-};
+// module.exports = {
+//   getAll,
+//   findById,
+//   create,
+//   update,
+//   deleteProduct,
+// };
