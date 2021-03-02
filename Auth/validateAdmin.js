ad9f8181c461f6c4ef217cb7787c0028e4baee4b
Isa Koga <isadorakoga710@gmail.com>
2021-03-02 16:11:31 -0300

    :rotating_light: change mongo url

--- Auth/validateAdmin.js
+++ Auth/validateAdmin.js
@@ -2,24 +2,28 @@ const jwt = require('jsonwebtoken');
 const { findUserByEmail } = require('../Services/usersService');
 const { secret } = require('../Controller/loginController');
 
+const fourHundredOne = 401;
+const fourHundredThree = 403;
+const fiveHundred = 500;
+
 const validateAdmin = async (req, res, next) => {
   const token = req.headers.authorization;
 
-  if (!token) return res.status(401).json({ message: 'missing auth token' });
+  if (!token) return res.status(fourHundredOne).json({ message: 'missing auth token' });
 
   try {
     const decoded = jwt.verify(token, secret);
     const user = await findUserByEmail(decoded.data.email);
 
     if (user.role !== 'admin') {
-      return res.status(403).json({ message: 'Only admins can register new admins' });
+      return res.status(fourHundredThree).json({ message: 'Only admins can register new admins' });
     }
 
     req.user = user;
 
     next();
   } catch (err) {
-    return res.status(500).json({ message: 'erro interno' });
+    return res.status(fiveHundred).json({ message: 'erro interno' });
   }
 };
 
