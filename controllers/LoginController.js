fd52b13fd8c48a68629f5931143a579e2460dea7
Júlia Pôrto <juportob@gmail.com>
2021-03-02 15:22:16 -0300

    feat: Requisito 2

---
+++ controllers/LoginController.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const jwt = require('jsonwebtoken');
+const { getByEmail } = require('../models/UsersModel');
+const validateLogin = require('../middlewares/LoginMid');
+
+const routerLogin = Router();
+const SUCCESS = 200;
+const secret = 'senha';
+const jwtConfig = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
+
+routerLogin.post('/', validateLogin, async (req, res) => {
+  const { email } = req.body;
+  const user = await getByEmail(email);
+  const payload = {
+    iss: 'Cookmaster', 
+    aud: 'identity',
+    // sub: user.name,
+    userData: user,
+  };
+  
+  const token = jwt.sign(payload, secret, jwtConfig);
+  return res.status(SUCCESS).json({ token });
+});
+
+module.exports = routerLogin;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routerController = require('./controllers/UsersController');
+const routerUsers = require('./controllers/UsersController');
+const routerLogin = require('./controllers/LoginController');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use('/users', routerController);
+app.use('/users', routerUsers);
+app.use('/login', routerLogin);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
