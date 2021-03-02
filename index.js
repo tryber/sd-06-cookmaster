e2958473872f95b651dc3c449aa86306e9faedb3
Anderson Regis <misterregisslv@gmail.com>
2021-03-02 15:39:41 -0300

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-// primeiro commit
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
