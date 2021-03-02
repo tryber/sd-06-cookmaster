a869ae31ce5bf5616c2448b5805ad7e33e3bd0f6
deko possas <andrepossaster@gmail.com>
2021-03-02 16:30:14 -0300

    criei o validate recipes

--- Services/recipesService.js
+++ Services/recipesService.js
@@ -13,10 +13,24 @@ const getRecipesById = async (id) => findOneRecipes(id);
 const putRecipe = async (id, name, ingr, prep) => editRecipes(id, name, ingr, prep);
 const delRecipe = async (id) => deleteRecipes(id);
 
+const BAD_REQUEST = 400;
+
+const validateRecipes = async (req, res, next) => {
+  const { name, ingredients, preparation } = req.body;
+
+  if (!name || !ingredients || !preparation) {
+    return res.status(BAD_REQUEST).json({
+      menssagem: 'Invalid entries. Try again.',
+    });
+  }
+  next();
+};
+
 module.exports = {
   createNewRecipe,
   getAllRecipes,
   getRecipesById,
   putRecipe,
   delRecipe,
+  validateRecipes,
 };
