a660633bb0b5dd84ee592a33b745023ec5ce6329
Anderson Regis <misterregisslv@gmail.com>
2021-03-02 15:15:20 -0300

    commit inicial projeto cookmaster Allez

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+// primeiro commit
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
