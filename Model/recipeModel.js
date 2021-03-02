c429405b43434ddedfb1b7ab7e3b42c3b0ed0452
deko possas <andrepossaster@gmail.com>
2021-03-02 16:00:37 -0300

    e ai torminha do barulho

--- Model/recipeModel.js
+++ Model/recipeModel.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const fundAllRecipes = async () => connection()
@@ -6,7 +7,11 @@ const fundAllRecipes = async () => connection()
 const createRecipes = async (data) => connection()
   .then((db) => db.connection('recipes').insertOne(data));
 
+const findRecipes = async (id) => connection()
+  .then((db) => db.connection('recipes').findOne(ObjectId(id)));
+
 module.exports = {
   fundAllRecipes,
   createRecipes,
+  findRecipes,
 };
