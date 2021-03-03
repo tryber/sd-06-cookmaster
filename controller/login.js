e5441b64cce25980e09a243e49439e5fd1e23bad
bynho25 <pablo.trybe@gmail.com>
2021-03-03 19:50:30 -0300

    Fixing

--- controller/login.js
+++ controller/login.js
@@ -5,28 +5,34 @@ const models = require('../models');
 
 const STATUS401 = 401;
 const STATUS200 = 200;
+const STATUS500 = 500;
 const login = Router();
 const secretPassword = 'Root2021';
 
 const jwtConfig = {
-  expiresIn: '30m',
+  expiresIn: '15d',
   algorithm: 'HS256',
 };
 
-login.post('/', rescue(async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(STATUS401).json({ message: 'All fields must be filled' });
+login.post('/', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(STATUS401).json({ message: 'All fields must be filled' });
+    }
+    const usersByEmail = await models.users.findByEmail(email);
+    if (!usersByEmail || usersByEmail.password !== password) {
+      return res.status(STATUS401).json({ message: 'Incorrect username or password' });
+    }
+    delete usersByEmail.password;
+    const payload = {
+      iss: 'post_api', aud: 'identify', user,
+    };
+    const token = jwt.sign(payload, secretPassword, jwtConfig);
+    res.status(STATUS200).json({ token });
+  } catch (err) {
+    return res.status(STATUS500).json({ message: `Intern Error: ${err}` });
   }
-  const usersByEmail = await models.users.findByEmail(email);
-  if (!usersByEmail || usersByEmail.password !== password) {
-    return res.status(STATUS401).json({ message: 'Incorrect username or password' });
-  }
-  const { password: _, ...userWithoutPassword } = usersByEmail;
-  const payload = { userWithoutPassword };
-  const token = jwt.sign(payload, secretPassword, jwtConfig);
-  res.status(STATUS200).json({ token });
-}));
+});
 
 module.exports = login;
