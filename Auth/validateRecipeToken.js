408505411cca2c19fa1bb3c8695a7899952c85d7
deko possas <andrepossaster@gmail.com>
2021-03-02 21:12:00 -0300

    isso ai

---
+++ Auth/validateRecipeToken.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const { findUserByEmail } = require('../Model/usersModel');
+
+const UNAUTHORIZED = 401;
+const secret = 'SARTOBOY';
+
+const validateRecipeToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(UNAUTHORIZED).json({
+    message:'missing auth token'
+  });
+  try {
+    const decode = jwt.verify(token, secret);
+    const user = await findUserByEmail(decoded.data.email);
+
+    if (!user) return res.status(UNAUTHORIZED).json({ message: 'jwt malformed' });
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    return res.status(UNAUTHORIZED).json({ message: 'jwt malformed' });
+  }
+};
