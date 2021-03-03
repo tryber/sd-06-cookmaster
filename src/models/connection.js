b3a25bdcba76a04de045e170924523b9cb28166b
Lais Gurgel <laisgurgelf@gmail.com>
2021-03-03 20:02:34 -0300

    Changing MONGO_DB_URL

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017';
-const MONGO_DB_URL = 'mongodb://localhost:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017';
+// const MONGO_DB_URL = 'mongodb://localhost:27017';
 const DATABASE = 'Cookmaster';
 
 const connection = async () => MongoClient.connect(MONGO_DB_URL, {
