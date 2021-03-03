b77d5608d65ed09744f8c99992ea0fd57a05c9fd
Cássio Rubens <cassiorubensadv@gmail.com>
2021-03-02 21:02:10 -0300

    FIX: MONGO_DB_URL

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,9 @@
 const { MongoClient } = require('mongodb');
 
 // ** conexão do banco local ** 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; 
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; 
 // ** conexão do banco para o avaliador funcionar **
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
