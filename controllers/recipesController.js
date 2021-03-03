c1e661d806a6876fd71101679aaa8e792468c3dd
jederson <jmacedo.ti@gmail.com>
2021-03-03 18:54:18 -0300

    Requisito 05

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -4,6 +4,8 @@ const {
   validateToken,
   recipeCreate,
   getAllRecipes,
+  getRecipeById,
+  validateId,
 } = require('../services/recipesServices');
 const { findOneUser } = require('../models/usersModel');
 
@@ -26,6 +28,14 @@ recipesRouter.get('/', async (_req, res) => {
   const recipes = await getAllRecipes();
 
   res.status(200).json(recipes);
+});
+
+recipesRouter.get('/:id', validateId, async (req, res) => {
+  const { id } = req.params;
+  const recipe = await getRecipeById(id);
+  if (!recipe) return res.status(404).json({ message: 'recipe not found' });
+
+  res.status(200).json(recipe);
 });
 
 module.exports = { recipesRouter };
