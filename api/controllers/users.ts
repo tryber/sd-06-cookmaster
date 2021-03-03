a69bdc529f0403ae2b0fe7707c332d126bd76255
pa-volpin <paulo.volpin@gmail.com>
2021-03-03 19:48:20 -0300

    feat: post user admin

--- api/controllers/users.ts
+++ api/controllers/users.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { checkToken } from '../auth/checkToken';
 import { getAll, getById, create, remove, update } from '../models/users'
 import {
   checkIfExist,
@@ -8,6 +9,30 @@ import {
 } from '../services/basicChecks'
 
 const usersController = Router();
+
+usersController.post('/admin', checkToken, (req, res) => {
+  const userRole = req.user.role;
+
+  if (userRole !== 'admin') {
+    const errorMsg = { message: 'Only admins can register new admins' };
+    return res.status(403).json(errorMsg)
+  }
+
+
+  const { name, email, password } = req.body;
+  const entriesAreValid = checkIfExist(email) && checkEmailIsValid(email) &&
+    checkNameIsValid(name) && checkIfExist(password) && checkPasswordIsValid(password);
+
+  if (!entriesAreValid) {
+    const errorMsg = { message: "Invalid entries. Try again." };
+    res.status(400).json(errorMsg)
+  } else {
+    const role = 'admin'
+    create({ name, email, password, role })
+      .then(r => res.status(201).json({ user: r }))
+      .catch(error => res.status(409).json(error))
+  }
+})
 
 usersController.post('/', (req, res) => {
   const { name, email, password } = req.body;
