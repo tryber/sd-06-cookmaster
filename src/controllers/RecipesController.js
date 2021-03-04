2f8d86332e05f10d908aef93278d94dbdbd036a4
rafMewtwo <ponciano.raf@gmail.com>
2021-03-03 21:04:54 -0300

    start projectx2

---
+++ src/controllers/RecipesController.js
@@ -0,0 +1,92 @@
+const { Router } = require('express');
+const rescue = require('express-rescue');
+
+const RecipesRouter = Router();
+const bodyParser = require('body-parser');
+
+RecipesRouter.use(bodyParser.json());
+
+// services imports
+const CreateRecipesService = require('../services/CreateRecipesService');
+const GetAllRecipesService = require('../services/GetAllRecipesService');
+const GetRecipesByIdService = require('../services/GetRecipesByIdService');
+const UpdateRecipesByIdService = require('../services/UpdateRecipesByIdService');
+const DeleteRecipesByIdService = require('../services/DeleteRecipesByIdService');
+const UpdateRecipesWithImageService = require('../services/UpdateRecipesWithImageService');
+
+// middleware imports
+const verfifyAuthorization = require('../middlewares/verfifyAuthorization');
+const verifyToken = require('../middlewares/verifyToken');
+const validateName = require('../middlewares/validateName');
+const validateToken = require('../auth/validateToken');
+const validateIngredients = require('../middlewares/validateIngredients');
+const validatePreparation = require('../middlewares/validatePreparation');
+const validateRecipeId = require('../middlewares/validateRecipeId');
+
+const CreateRecipes = async (req, res) => {
+  const { name, ingredients, preparation } = req.body;
+  const { authorization } = req.headers;
+
+  const payload = validateToken(authorization);
+  const { _id } = payload;
+  const resp = await CreateRecipesService(name, ingredients, preparation, _id);
+
+  return res.status(resp.status).json(resp.mensage);
+};
+
+const getAllRecipes = async (_req, res) => {
+  const resp = await GetAllRecipesService();
+  return res.status(resp.status).json(resp.recipesResponse);
+};
+
+const getRecipesById = async (req, res) => {
+  const { id } = req.params;
+  const resp = await GetRecipesByIdService(id);
+  return res.status(resp.status).json(resp.recipesResponse);
+};
+
+const updateRecipesById = async (req, res) => {
+  const { name, ingredients, preparation } = req.body;
+  const { id } = req.params;
+
+  const resp = await UpdateRecipesByIdService(id, name, ingredients, preparation);
+  return res.status(resp.status).json(resp.recipesResponse);
+};
+
+const deleteRecipesById = async (req, res) => {
+  const { id } = req.params;
+  const resp = await DeleteRecipesByIdService(id);
+  return res.status(resp.status).json();
+};
+
+RecipesRouter.post(
+  '/', validateName, validateIngredients, validatePreparation, verfifyAuthorization, rescue(
+    CreateRecipes,
+    ),
+);
+RecipesRouter.get('/', rescue(getAllRecipes));
+
+// multer
+const UploadSingle = require('../middlewares/uploadSingle');
+
+const uploadImage = async (req, res) => {
+  const { id } = req.params;
+
+  const resp = await UpdateRecipesWithImageService(id);
+  return res.status(resp.status).json(resp.recipesResponse);
+};
+
+RecipesRouter.put(
+  '/:id/image/',
+  verifyToken,
+  verfifyAuthorization,
+  UploadSingle,
+  uploadImage,
+);
+// end multer
+
+RecipesRouter.get('/:id', validateRecipeId, rescue(getRecipesById));
+RecipesRouter.put('/:id', verifyToken, verfifyAuthorization, rescue(updateRecipesById));
+RecipesRouter.delete('/:id', verifyToken, verfifyAuthorization, rescue(deleteRecipesById));
+
+module.exports = RecipesRouter;
