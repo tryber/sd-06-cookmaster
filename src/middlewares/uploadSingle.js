c52b35d6d0542986911cba94309944c6a17aa4eb
rafMewtwo <ponciano.raf@gmail.com>
2021-03-04 14:53:26 -0300

    start projectx10

--- src/middlewares/uploadSingle.js
+++ src/middlewares/uploadSingle.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, '../..', 'uploads'),
+  destination: path.resolve(__dirname, '..', '..', 'uploads'),
   filename: (req, file, callback) => {
     // callback(null, `${Date.now()}-${file.originalname}`);
     callback(null, `${req.params.id}.jpeg`);
