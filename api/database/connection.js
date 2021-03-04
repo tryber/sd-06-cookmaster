d7b9b84351a6a45e21bad6ba8056ea41fa13b242
Leandro <leandro.feitoza8018@gmail.com>
2021-03-04 14:54:27 -0300

    -m

--- api/database/connection.js
+++ api/database/connection.js
@@ -1,6 +1,6 @@
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-//const DB_NAME = 'Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 const { MongoClient } = require('mongodb');
 
