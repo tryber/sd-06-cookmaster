8bf91028a62f042ab6ee5873d1acbd7bfab8a888
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2021-03-05 14:53:40 -0300

    Iniciando o projeto cookmaster

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
