a7d9fc6d959b3b96112f0a9288ef1deb27e0e3e8
rafMewtwo <ponciano.raf@gmail.com>
2021-03-05 09:15:22 -0300

    start projectx12

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const rescue = require('express-rescue');
 
 const app = express();
+
 const PORT = 3000;
 const ERROR = 500;
+
 const UsersController = require('./src/controllers/UsersController');
 const LoginController = require('./src/controllers/LoginController');
 const RecipesController = require('./src/controllers/RecipesController');
@@ -15,33 +16,33 @@ app.get('/', (request, response) => {
 });
 // nao remova o endpoint acima
 // rota de impressao
-app.use((req, res, next) => {
-  res.on('finish', () => console.log({
-    date: new Date(),
-    req: {
-      host: req.get('host'),
-      method: req.method,
-      endPoint: req.originalUrl,
-      data: {
-        headers: JSON.stringify(req.headers, 0, 2),
-        body: JSON.stringify(req.body, 0, 2),
-        params: JSON.stringify(req.params, 0, 2),
-        query: JSON.stringify(req.query, 0, 2),
-      },
-    },
-    res: {
-      status: res.statusCode,
-    },
-  }));
-  next();
-});
+// app.use((req, res, next) => {
+//   res.on('finish', () => console.log({
+//     date: new Date(),
+//     req: {
+//       host: req.get('host'),
+//       method: req.method,
+//       endPoint: req.originalUrl,
+//       data: {
+//         headers: JSON.stringify(req.headers, 0, 2),
+//         body: JSON.stringify(req.body, 0, 2),
+//         params: JSON.stringify(req.params, 0, 2),
+//         query: JSON.stringify(req.query, 0, 2),
+//       },
+//     },
+//     res: {
+//       status: res.statusCode,
+//     },
+//   }));
+//   next();
+// });
 // end rota de impressao
 // deixando a imagem disponivel de volta com rota dinamica
 app.use('/images', express.static(`${__dirname}/uploads`));
 
-app.use('/users', rescue(UsersController));
-app.use('/login', rescue(LoginController));
-app.use('/recipes', rescue(RecipesController));
+app.use('/users', UsersController);
+app.use('/login', LoginController);
+app.use('/recipes', RecipesController);
 
 app.use((error, req, res, _next) => {
   console.log({ error });
