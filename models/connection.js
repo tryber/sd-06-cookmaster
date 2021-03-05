d03421c1c28ece7c578bc0f68ec187a5a83b0215
Rodrigo Leite <rods.leite27@gmail.com>
2021-03-05 08:42:29 -0300

    eslint try 1

--- models/connection.js
+++ models/connection.js
@@ -3,8 +3,8 @@ const { MongoClient } = require('mongodb');
 let connection;
 
 const DB_NAME = 'Cookmaster';
-const DB_URI = `mongodb://localhost:27017${DB_NAME}`;
-// const EVALUATOR_URI = `mongodb://mongodb:27017${DB_NAME}`;
+// const DB_URI = `mongodb://localhost:27017${DB_NAME}`;
+const DB_URI = `mongodb://mongodb:27017${DB_NAME}`;
 
 module.exports = async function (collection) {
   connection = connection || (await MongoClient.connect(DB_URI, {
