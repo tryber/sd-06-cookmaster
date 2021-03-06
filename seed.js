443b8726cafdf40530bbb63b4e6373e3c7079372
ricardorosa-dev <ricardorosa@gmail.com>
2021-03-06 19:57:54 -0300

    Requisito 6

--- seed.js
+++ seed.js
@@ -1,1 +1,11 @@
-// colocar query do MongoDB
+const connection = require('./src/models/connection');
+
+connection().then((db) => db.collection('users')
+  .insertOne({
+    name: 'admin', 
+    email: 'root@email.com', 
+    password: 'admin', 
+    role: 'admin',
+  }));
+
+// db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
