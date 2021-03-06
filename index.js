4b42e547baafebd80022843309392a018cfd79f8
ricardorosa-dev <ricardorosa@gmail.com>
2021-03-06 13:52:39 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const UsersController = require('./src/controllers/UsersController');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use('/users', UsersController);
+
+app.use((err, req, res, _next) => 
+  res.status(500).json({ message: 'Erro interno' }));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
-  response.send();
+  response.send('ok');
 });
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
---
+++ src/models/Users.js
@@ -0,0 +1,33 @@
+// const { ObjectId } = require('mongodb');
+const connection = require('./connection');
+
+const getAll = async () =>
+  connection().then((db) => db.collection('users')
+    .find().toArray());
+    
+const findByEmail = async (email) => 
+  connection().then((db) => db.collection('users')
+    .findOne({ email }));
+
+const create = async (name, email, password, role) => {
+  const { insertedId } = await connection().then((db) => db.collection('users')
+    .insertOne({
+      name,
+      email,
+      password,
+      role,
+    }));
+  
+    return {
+      name,
+      email,
+      role,
+      _id: insertedId,
+    };
+};
+
+module.exports = {
+  getAll,
+  findByEmail,
+  create,
+};
