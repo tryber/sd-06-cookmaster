b2e4ead0519936c9e18047555954baea0dd769c5
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-03-06 18:13:51 -0300

    fix: remove incorrect route

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use('/ping', (request, response) => {
 app.use('/users', UsersController);
 app.use('/login', LoginController);
 app.use('/recipes', RecipesController);
-app.use('/recipes/:id', RecipesController);
 
 app.use((error, request, response, _next) => {
   response.status(error.code).json(error.errorMessage);
