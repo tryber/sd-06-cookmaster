5a99d98c508385809bba87f49df06fb03a49f8ce
Pedro Duarte <ikbenpedro@gmail.com>
2021-03-09 16:55:11 -0300

    Projeto JWT - Nodejs e MongoDB || Pedro Duarte

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const port =3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`knocking on the door ${port}`)
+})
