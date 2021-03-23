f48118a4da88cfbc20f5725c1db6ee2ef07f1b5b
Anderson <and.stuber@gmail.com>
2021-03-23 19:28:56 -0300

    Lint fixes index.js: Linha 5: Expected 1 empty line after require statement not followed by another require. Linha 17: More than 1 blank line not allowed. Linha 23: More than 1 blank line not allowed.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const path = require('path');
+
 app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -14,12 +15,10 @@ const { UsersRouter } = require('./src/Controllers/usersController');
 const { LoginRouter } = require('./src/Controllers/loginController');
 const { RecipesRouter } = require('./src/Controllers/recipesController');
 
-
 app.use('/users', UsersRouter);
 app.use('/login', LoginRouter);
 app.use('/recipes', RecipesRouter);
 app.use('/images', express.static(path.join(__dirname, 'uploads')));
-
 
 const port = 3000;
 app.listen(port, () => console.log(`Running on port -> ${port}`));
