b2f4fe973a313e47f695dbd4e22c88aace3f7592
Anderson <and.stuber@gmail.com>
2021-03-23 01:21:43 -0300

    Ajuste para o requisito 9, adicionado '/images'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+const path = require('path');
 app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -18,6 +18,8 @@ const { RecipesRouter } = require('./src/Controllers/recipesController');
 app.use('/users', UsersRouter);
 app.use('/login', LoginRouter);
 app.use('/recipes', RecipesRouter);
+app.use('/images', express.static(path.join(__dirname, '/images')));
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Running on port -> ${port}`));
