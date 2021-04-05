54b23d9766dabfcd32dbb069cc46e53d48695c3f
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-04-04 22:39:48 -0300

    ..

--- Services/users.js
+++ Services/users.js
@@ -1,8 +1,11 @@
 const usersModel = require('../Models/users');
 
-const addUser = async (userInfo) => usersModel.addUser(userInfo);
 const Error400 = 400;
 const Error409 = 409;
+
+const addUser = async (userInfo) => usersModel.addUser(userInfo);
+const getOneUser = async (info) => usersModel.getOneUser(info);
+
 const validateEmail = (email) => {
   const emailRegex = /\S+@\S+.\S+/;
   return emailRegex.test(email);
@@ -10,7 +13,7 @@ const validateEmail = (email) => {
 
 const validateUser = async (req, res, next) => {
   const { name, email, password } = req.body;
-  if (!name || !email || !validateEmail(email) || !password) {
+  if (!name || !email || !password || !validateEmail(email)) {
     return res.status(Error400).json({ message: 'Invalid entries. Try again.' });
   }
   next();
@@ -32,4 +35,5 @@ module.exports = {
   addUser,
   validateUser,
   validateUniqueEmail,
+  getOneUser,
 };
