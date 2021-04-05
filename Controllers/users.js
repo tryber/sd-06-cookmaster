864f73a884a99d6b8fe45f6684dc067227a56307
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-04-04 22:26:37 -0300

    .

--- Controllers/users.js
+++ Controllers/users.js
@@ -10,7 +10,7 @@ const {
 
 const S201 = 201;
 
-usersRouter.post('./users', validateUser, validateUniqueEmail, async (req, res) => {
+usersRouter.post('/', validateUser, validateUniqueEmail, async (req, res) => {
   const newUser = { ...req.body, role: 'user' };
   await addUser(newUser);
   return res.status(S201).json({ newUser });
