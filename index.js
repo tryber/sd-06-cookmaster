85b7df95d04badbe13b70c78b644abf315e2e060
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-04-04 22:22:45 -0300

    fix: appUse user

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express();
 const usersRouter = require('./Controllers/users');
 
 app.use(bodyParser.json());
-app.use('/users', usersRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+app.use('/users', usersRouter);
 
 app.listen(port, () => console.log('pai ta on'));
